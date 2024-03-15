d422937f34533f11d04d654d4141d47dea7b7877
Hari Dass <hari.dass@mail.vinove.com>
2024-03-15 14:14:38 +0530

    first commit

---
+++ packages/flutter_background_service_android/android/src/main/java/id/flutter/flutter_background_service/BackgroundService.java
@@ -0,0 +1,428 @@
+package id.flutter.flutter_background_service;
+
+import static android.os.Build.VERSION.SDK_INT;
+
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ServiceInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.os.SystemClock;
+import android.util.Log;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationListener;
+
+import android.location.Criteria;
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.ServiceCompat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import io.flutter.FlutterInjector;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.dart.DartExecutor;
+import io.flutter.embedding.engine.loader.FlutterLoader;
+import io.flutter.plugin.common.JSONMethodCodec;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.PluginRegistry;
+
+public class BackgroundService extends Service implements MethodChannel.MethodCallHandler, LocationListener{
+
+    private static final String TAG = "BackgroundService";
+    private static final String LOCK_NAME = BackgroundService.class.getName()
+            + ".Lock";
+    public static volatile WakeLock lockStatic = null; // notice static
+    AtomicBoolean isRunning = new AtomicBoolean(false);
+    private FlutterEngine backgroundEngine;
+    private MethodChannel methodChannel;
+    private Config config;
+    private DartExecutor.DartEntrypoint dartEntrypoint;
+    private boolean isManuallyStopped = false;
+    private String notificationTitle;
+    private String notificationContent;
+    private String notificationChannelId;
+    private int notificationId;
+    private Handler mainHandler;
+    private int MIN_DISTANCE =10;
+    private int MIN_UPDATE_TIME= 10000;
+    synchronized public static PowerManager.WakeLock getLock(Context context) {
+        if (lockStatic == null) {
+            PowerManager mgr = (PowerManager) context
+                    .getSystemService(Context.POWER_SERVICE);
+            lockStatic = mgr.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                    LOCK_NAME);
+            lockStatic.setReferenceCounted(true);
+        }
+
+        return lockStatic;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+ @Override
+    public void onLocationChanged(Location updatedLocation) {
+        // We received a location update on a separate thread!
+         Log.w(TAG,  "onLocationChanged "+ updatedLocation.toString());
+        location = updatedLocation;
+        // You can verify which thread you're on by something like this:
+        // Log.d("Which thread?", Thread.currentThread() == Looper.getMainLooper().getThread() ? "UI Thread" : "New thread");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.w(TAG,  "onStatusChanged: provider " +provider);
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.w(TAG,  "onProviderEnabled:" +provider);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+    
+  Location location;
+  LocationManager   locationManager ; 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        FlutterBackgroundServicePlugin.servicePipe.addListener(listener);
+        Log.w(TAG, "onCreate" );  
+        config = new Config(this);
+        mainHandler = new Handler(Looper.getMainLooper());
+
+        String notificationChannelId = config.getNotificationChannelId();
+        if (notificationChannelId == null) {
+            this.notificationChannelId = "FOREGROUND_DEFAULT";
+            createNotificationChannel();
+        } else {
+            this.notificationChannelId = notificationChannelId;
+        }
+
+        notificationTitle = config.getInitialNotificationTitle();
+        notificationContent = config.getInitialNotificationContent();
+        notificationId = config.getForegroundNotificationId();
+        updateNotificationInfo();
+        onStartCommand(null, -1, -1);
+        Log.w(TAG, "locationmanager init" );  
+        locationManager = getSystemService(LocationManager.class);
+        getCurrentLocation();    
+
+        
+    }
+    public String getCurrentLocation(){
+            if(locationManager != null  ) {
+                
+                    Log.w(TAG, "getCurrentLocation  " );  
+                    Criteria cri = new Criteria();  
+                    String provider = locationManager.getBestProvider(cri,false);  
+                    Log.w(TAG, "locationManager provider " + provider );  
+                    if(provider!=null & !provider.equals(""))  
+                    {  
+                        location = locationManager.getLastKnownLocation(provider);  
+                       try {
+                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10, this);
+                        }
+                        catch(SecurityException e){
+                            // The app doesn't have the correct permissions
+                        }
+
+                        if(location!=null)  
+                        {  
+                        Log.w(TAG, "getCurrentLocation: "  +   location.toString());
+                        }  
+                        else{  
+                                Log.w(TAG, "location not found" );  
+                        }  
+                    }  
+                    else  
+                    {  
+                    Log.w(TAG, "Provider is null" );  
+                    }
+            }
+            return null!=location? location.toString():null;
+    }
+    @Override
+    public void onDestroy() {
+        if (!isManuallyStopped) {
+            WatchdogReceiver.enqueue(this);
+        } else {
+            config.setManuallyStopped(true);
+        }
+        stopForeground(true);
+        isRunning.set(false);
+
+        if (backgroundEngine != null) {
+            backgroundEngine.getServiceControlSurface().detachFromService();
+            backgroundEngine.destroy();
+            backgroundEngine = null;
+        }
+
+        FlutterBackgroundServicePlugin.servicePipe.removeListener(listener);
+        methodChannel = null;
+        dartEntrypoint = null;
+        super.onDestroy();
+    }
+
+    private final Pipe.PipeListener listener = new Pipe.PipeListener() {
+        @Override
+        public void onReceived(JSONObject object) {
+            receiveData(object);
+        }
+    };
+
+    private void createNotificationChannel() {
+        if (SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Background Service";
+            String description = "Executing process in background";
+
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel(notificationChannelId, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    protected void updateNotificationInfo() {
+        if (config.isForeground()) {
+            String packageName = getApplicationContext().getPackageName();
+            Intent i = getPackageManager().getLaunchIntentForPackage(packageName);
+
+            int flags = PendingIntent.FLAG_CANCEL_CURRENT;
+            if (SDK_INT >= Build.VERSION_CODES.S) {
+                flags |= PendingIntent.FLAG_MUTABLE;
+            }
+
+            PendingIntent pi = PendingIntent.getActivity(BackgroundService.this, 11, i, flags);
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, notificationChannelId)
+                    .setSmallIcon(R.drawable.ic_bg_service_small)
+                    .setAutoCancel(true)
+                    .setOngoing(true)
+                    .setContentTitle(notificationTitle)
+                    .setContentText(notificationContent)
+                    .setContentIntent(pi);
+
+            try {
+                ServiceCompat.startForeground(this, notificationId, mBuilder.build(), ServiceInfo.FOREGROUND_SERVICE_TYPE_MANIFEST);
+            } catch (SecurityException e) {
+              Log.w(TAG, "Failed to start foreground service due to SecurityException - have you forgotten to request a permission? - " + e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        config.setManuallyStopped(false);
+        WatchdogReceiver.enqueue(this);
+        runService();
+
+        return START_STICKY;
+    }
+
+    @SuppressLint("WakelockTimeout")
+    private void runService() {
+        try {
+            if (isRunning.get() || (backgroundEngine != null && !backgroundEngine.getDartExecutor().isExecutingDart())) {
+                Log.v(TAG, "Service already running, using existing service");
+                return;
+            }
+
+            Log.v(TAG, "Starting flutter engine for background service");
+            getLock(getApplicationContext()).acquire();
+
+            updateNotificationInfo();
+
+            FlutterLoader flutterLoader = FlutterInjector.instance().flutterLoader();
+            // initialize flutter if it's not initialized yet
+            if (!flutterLoader.initialized()) {
+                flutterLoader.startInitialization(getApplicationContext());
+            }
+
+            flutterLoader.ensureInitializationComplete(getApplicationContext(), null);
+
+            isRunning.set(true);
+            backgroundEngine = new FlutterEngine(this);
+
+            // remove FlutterBackgroundServicePlugin (because its only for UI)
+            backgroundEngine.getPlugins().remove(FlutterBackgroundServicePlugin.class);
+
+            backgroundEngine.getServiceControlSurface().attachToService(BackgroundService.this, null, config.isForeground());
+
+            methodChannel = new MethodChannel(backgroundEngine.getDartExecutor().getBinaryMessenger(), "id.flutter/background_service_android_bg", JSONMethodCodec.INSTANCE);
+            methodChannel.setMethodCallHandler(this);
+
+            dartEntrypoint = new DartExecutor.DartEntrypoint(flutterLoader.findAppBundlePath(), "package:flutter_background_service_android/flutter_background_service_android.dart", "entrypoint");
+
+            final List<String> args = new ArrayList<>();
+            long backgroundHandle = config.getBackgroundHandle();
+            args.add(String.valueOf(backgroundHandle));
+
+
+            backgroundEngine.getDartExecutor().executeDartEntrypoint(dartEntrypoint, args);
+
+        } catch (UnsatisfiedLinkError e) {
+            notificationContent = "Error " + e.getMessage();
+            updateNotificationInfo();
+
+            Log.w(TAG, "UnsatisfiedLinkError: After a reboot this may happen for a short period and it is ok to ignore then!" + e.getMessage());
+        }
+    }
+
+    public void receiveData(JSONObject data) {
+        if (methodChannel == null) return;
+        try {
+            final JSONObject arg = data;
+            mainHandler.post(() -> {
+                if (methodChannel == null) return;
+                methodChannel.invokeMethod("onReceiveData", arg);
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        if (isRunning.get()) {
+            WatchdogReceiver.enqueue(getApplicationContext(), 1000);
+        }
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+        String method = call.method;
+
+        try {
+            if (method.equalsIgnoreCase("setNotificationInfo")) {
+                JSONObject arg = (JSONObject) call.arguments;
+                if (arg.has("title")) {
+                    notificationTitle = arg.getString("title");
+                    notificationContent = arg.getString("content");
+                    updateNotificationInfo();
+                    result.success(true);
+                }
+                return;
+            }
+
+            if (method.equalsIgnoreCase("setAutoStartOnBootMode")) {
+                JSONObject arg = (JSONObject) call.arguments;
+                boolean value = arg.getBoolean("value");
+                config.setAutoStartOnBoot(value);
+                result.success(true);
+                return;
+            }
+
+            if (method.equalsIgnoreCase("setForegroundMode")) {
+                JSONObject arg = (JSONObject) call.arguments;
+                boolean value = arg.getBoolean("value");
+                config.setIsForeground(value);
+                if (value) {
+                    updateNotificationInfo();
+                    backgroundEngine.getServiceControlSurface().onMoveToForeground();
+                } else {
+                    stopForeground(true);
+                    backgroundEngine.getServiceControlSurface().onMoveToBackground();
+                }
+
+                result.success(true);
+                return;
+            }
+
+            if (method.equalsIgnoreCase("isForegroundMode")) {
+                boolean value = config.isForeground();
+                result.success(value);
+                return;
+            }
+
+            if (method.equalsIgnoreCase("getCurrentPosition")) {
+                getCurrentLocation(); 
+                double longitude = location.getLongitude();
+                double latitude = location.getLatitude();
+                String value= "lat"  + latitude + ", lng:" + longitude ;
+                Log.w(TAG,  "getCurrentLocation response "+ value);  
+                result.success(value);
+                return;
+            }
+
+
+            if (method.equalsIgnoreCase("stopService")) {
+                isManuallyStopped = true;
+                WatchdogReceiver.remove(this);
+                stopSelf();
+                result.success(true);
+                return;
+            }
+
+            if (method.equalsIgnoreCase("sendData")) {
+                try {
+                    if (FlutterBackgroundServicePlugin.mainPipe.hasListener()){
+                        FlutterBackgroundServicePlugin.mainPipe.invoke((JSONObject) call.arguments);
+                    }
+
+                    result.success(true);
+                } catch (Exception e) {
+                    result.error("send-data-failure", e.getMessage(), e);
+                }
+                return;
+            }
+
+            if(method.equalsIgnoreCase("openApp")){
+                try{
+                    String packageName=  getPackageName();
+                    Intent launchIntent= getPackageManager().getLaunchIntentForPackage(packageName);
+                    if (launchIntent != null) {
+                        launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                        startActivity(launchIntent);
+                        result.success(true);
+
+                    }
+                }catch (Exception e){
+                    result.error("open app failure", e.getMessage(),e);
+
+                }
+                return;
+
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, e.getMessage());
+            e.printStackTrace();
+        }
+
+        result.notImplemented();
+    }
+}
